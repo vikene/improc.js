bd19398f623bf6e1d0d675e979c3ceee7bf5881e
Vigneash Sundar <vigneashsundar@live.com>
2014-10-09 21:50:47 -0700

    Object oriented design
    ImProc. js, end Js design. Experiments Ended

---
+++ js/improc.js
@@ -0,0 +1,15 @@
+/* ImProc javascript library to perform real time image processing in Web browser
+    or any HTML 5 enables device. 
+    This  is the main file which is for image processing.
+    
+    
+    */ 
+
+var improC = improC || {};
+
+var improC.evenT = {};
+var improC.filteR = {};
+var improC.convolutioN = {};
+var improC.defaultS = {};
+
+
