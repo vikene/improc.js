fbe14dedfb5565e19a12c42bbf3a043977f599f9
Vigneash Sundar <vigneashsundar@live.com>
2014-10-11 01:16:43 -0700

    filter signatures
    ToDO listing

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,20 @@
 improc.filteR = {
 
 /* 
-Filter definitions for improc */ 
+Filter definitions for improc.js list of filters are given in wiki  */ 
+    
+    gblur:
+    
+    embross:
+    embross2:
+    sharpen:
+    edge:
+    edge2:
+    redfilter:
+    greenfilter:
+    bluefilter:
+    Grayscale:
+    imageflip:
+    
 
 }
