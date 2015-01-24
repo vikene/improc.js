3d0e8f8af655bb3f9e2d982123e9b0223082809c
Vigneash Sundar <vigneashsundar@live.com>
2015-01-25 01:48:41 +0530

    Added main library reflection

---
+++ js/convolution.js
@@ -0,0 +1,11 @@
+improc.convolutioN = {
+/* Convolution matrix for improc */
+    
+    convolution3x3: function(){},
+    
+    convolution5x5: function(){},
+    
+    /* for high quality of point processing, may become laggy when run on high resolution image */ 
+    convolution9x9: function(){}
+
+}
---
+++ js/filter.js
@@ -0,0 +1,37 @@
+improc.filteR = {
+
+/* 
+Filter definitions for improc.js list of filters are given in wiki  */ 
+    
+    gblur: function()
+    {
+    },
+    
+    embross: function()
+    {
+},
+    embross2: function()
+{
+},
+    sharpen: function()
+{
+},
+
+    edge: function()
+{
+},
+    edge2: function()
+{
+},
+
+    redfilter: function()
+{},
+    greenfilter: function(){},
+    bluefilter: function(){},
+    Grayscale: function(){},
+    imageflip: function(){},
+    snoweffect: function(){},
+    invert: function(){}
+    
+
+}
